c86ad9f5a793ae6e6f0a1e4827033ffe797e2a3f
leah duvic <leaheduvic@gmail.com>
2017-11-17 19:16:48 -0600

    printed to DOM, still more to do

--- store.js
+++ store.js
@@ -1,34 +1,36 @@
 $(document).ready(function () {
-    const outputEl = $(".product-listing")
-    // you are requesting access to these files here, so you can use them as you go. 
-    $.ajax({
-        "url": "products.json",
-        "method": "GET"
-        // all the products.json is nested with in productData.
-    }).then(productData => {
+	const outputEl = $(".product-listing")
+	
+	// you are requesting access to these files here, so you can use them as you go. 
+	$.ajax({
+		"url": "products.json",
+		"method": "GET"
+		// all the products.json is nested with in productData.
+	}).then(productData => {
+		let totalPrint = ""
         
-        const products = productData.products 
+		const productInfo = productData.products 
 
-        let saleInfo = "<section id='saleContainer'>"
+		let saleInfo = "<section id='saleContainer'></section"
         
-        $.ajax({
-            "url": "categories.json",
-            "method": "GET"
-        }).then (categoryData => {
-            let totalPrint = ""
+		$.ajax({
+			"url": "categories.json",
+			"method": "GET"
+		}).then (categoryData => {
 
-            const categories = categoryData.categories
-            products.forEach(sale => {
-                const productCategory = categories.find(current => current.id === product.category_id)
-                totalPrint += `
-                <article id="product_${product.id}>
-                    <p>Name: ${product.name}</p>
-                    <p>Category: ${productCategory.name}</p>
-                    <p>Price: ${product.price}</p>
+			const categoryInfo = categoryData.categories
+
+			productInfo.forEach(sale => {
+				// const productCategory = categoryInfo.find(current => current.id === categoryData.category_id)
+				totalPrint += `
+                <article id="product_${sale.id}>
+				<p>Name: ${sale.name}</p>
+				<p>Category: ${categoryInfo.name}</p>
+				<p>Price: ${sale.price}</p>
                 </article>
-            `  
-            })
-            outputEl.html(totalPrint)
-        })
-    })
+				`  
+			})
+			outputEl.html(totalPrint)
+		})
+	})
 })
